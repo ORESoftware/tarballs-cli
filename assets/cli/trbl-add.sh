8ff473ae6896d0161b8719d8bb686f5b0632491d
Olegzandr VD <alex@oresoftware.com>
2018-05-27 00:25:38 -0700

    pdev:'set'

--- assets/cli/trbl-add.sh
+++ assets/cli/trbl-add.sh
@@ -11,7 +11,6 @@ file="$1";
 file_name="$2";
 
 
-
 if [ -z "$current_repo" ]; then
    echo "no current_repo -> try this '$ trbl use x'" >&2;
    exit 1;
@@ -25,6 +24,9 @@ if [ -z "$file" ]; then
    exit 1;
 fi
 
+if [[ "$file" != /* ]]; then
+    file="$PWD/$file"
+fi
 
 
 if [ -z "$file_name" ]; then
@@ -35,11 +37,14 @@ fi
 
 (
   set -e;
+
   my_repo="$HOME/.trbl/repos/$current_repo";
+
   cd "$my_repo" || {
      echo "There does not seem to be repo here: '$my_repo'" >&2;
      exit 1;
   }
+
   git fetch origin;
   git reset --hard "origin/master";
   mkdir -p "$(dirname "$file_name")"
